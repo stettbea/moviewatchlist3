3f8be8468aac52f7a9795dd999fd95642dcc3d49
stettbea <stettler.beatrice>
2026-04-14 21:24:02 +0200

    Netlifyeinbindung

--- movie-watchlist-3/svelte.config.js
+++ movie-watchlist-3/svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-netlify';
-
-export default {
-    kit: {
-        adapter: adapter({
-            edge: false,
-            split: false
-        })
-    }
+import adapter from '@sveltejs/adapter-netlify'; 
+ 
+export default { 
+    kit: { 
+        adapter: adapter({ 
+            edge: false, 
+            split: false 
+        }) 
+    } 
 };
